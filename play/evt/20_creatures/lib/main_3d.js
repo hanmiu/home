dd707bf8042f95d02c82b0ba3f305b194d1e2275
somi <somi@somi.local>
2020-07-21 19:23:29 +0900

    data

---
+++ play/evt/20_creatures/lib/main_3d.js
@@ -0,0 +1,289 @@
+let vs = `
+  uniform float time;
+
+  varying vec2 vUv;
+
+  void main()
+  {
+    vUv = uv;
+    vUv.y = (1.0 - uv.y);
+    vec4 p = vec4(position, 1.0);
+    vec2 uv = p.xy;
+    float k = sin(time*0.2)*0.5+0.5;
+    //k = pow(k, 2.0);
+    p.x += k * cos(uv.y * 3.14 * 5.0 - time * 10.0) * 0.1 * uv.y;
+
+    //vUv.x -= k * cos(uv.y * 3.14 * 10.0 - time * 10.0) * 0.01 * uv.y;
+
+    vec4 mvPosition = modelViewMatrix * p;
+    gl_Position = projectionMatrix * mvPosition;
+  }
+`;
+
+let fs = `
+  uniform float time;
+
+  uniform sampler2D colorTexture;
+  uniform sampler2D strokeTexture;
+  uniform vec3 base_color;
+
+  varying vec2 vUv;
+
+  void main( void ) {
+    vec2 position = - 1.0 + 2.0 * vUv;
+
+    float fill_mask = texture2D( colorTexture, vUv ).a;
+    vec4 stroke_tex = texture2D( strokeTexture, vUv );
+    float stroke_mask = stroke_tex.a;
+
+    vec3 a = vec3(cos(time*0.2)*0.5+0.5, sin(time)*0.5+0.5, vUv.y);
+    a = vec3(1.0*(0.5+0.5*sin(time)), 0.2, 0.3*cos(time*0.5));
+    float k = (1.0 - length(position - vec2(0.0, 0.4))) * 0.5;
+    k = clamp(k, 0.0, 1.0);
+    a += pow(k, 0.8);
+    vec3 b = vec3(0.2, 0.4, 0.2 + 0.5*sin(time));
+    b = b * sin(length(vUv) * 3.141592 * 5.0);
+
+    vec3 color = a * fill_mask * (1.0 - stroke_mask) + stroke_mask * b;
+    //vec3 color = vec3(1.0, 0.0, 0.0);
+    color = a * (1.0 - stroke_mask) + stroke_mask * b;
+    gl_FragColor = vec4( color, 1.0 );
+
+  }
+`;
+
+let fs1 = `
+  uniform float time;
+
+  uniform sampler2D colorTexture;
+  uniform sampler2D strokeTexture;
+  uniform vec3 base_color;
+
+  varying vec2 vUv;
+
+  void main( void ) {
+    vec2 position = - 1.0 + 2.0 * vUv;
+
+    float fill_mask = texture2D( colorTexture, vUv ).a;
+    vec4 stroke_tex = texture2D( strokeTexture, vUv );
+    //stroke_tex = texture2D( strokeTexture, gl_FragCoord.xy / 512.0);
+    float stroke_mask = stroke_tex.a;
+
+    vec3 a = vec3(cos(time*0.2)*0.5+0.5, sin(time)*0.5+0.5, vUv.y);
+    a = vec3(1.0*(0.5+0.5*sin(time)), 0.2, 0.3*cos(time*0.5));
+    float k = (1.0 - length(position - vec2(0.0, 0.4))) * 0.5;
+    k = clamp(k, 0.0, 1.0);
+    a += pow(k, 0.8);
+    vec3 b = vec3(0.2, 0.4, 0.2 + 0.5*sin(time));
+    b = b * sin(length(vUv) * 3.141592 * 5.0);
+
+    vec3 color = a * fill_mask * (1.0 - stroke_mask) + stroke_mask * b;
+    if(stroke_tex.a < 0.1) {
+      color = base_color * gl_FragCoord.y / 512.0;
+    }
+    else {
+      color = stroke_tex.rgb;
+    }
+
+    gl_FragColor = vec4( color, 1.0 );
+
+  }
+`;
+
+let xdoc;
+let im0, im1;
+
+let camera, scene, renderer;
+let msh, ext, uniforms;
+
+let dom_parser = new DOMParser();
+
+camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 3000 );
+camera.position.z = 3;
+
+scene = new THREE.Scene();
+
+renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+renderer.setPixelRatio( window.devicePixelRatio );
+camera.aspect = 372 / 626;
+camera.updateProjectionMatrix();
+renderer.setSize( 372, 626 );
+renderer.domElement.id = 'layer-3d';
+document.querySelector('#stage').appendChild( renderer.domElement );
+
+let texture = new THREE.Texture(im0);
+let texture1 = new THREE.Texture(im1);
+
+uniforms = {
+  "time": { value: Date.now() / 1000 },
+  "base_color": { value: new THREE.Color(1.0, 1.0, 1.0) },
+  "colorTexture": { value: texture },
+  "strokeTexture": { value: texture1 },
+};
+
+let material = new THREE.ShaderMaterial( {
+  uniforms: uniforms,
+  vertexShader: vs,
+  fragmentShader: fs,
+  side: THREE.DoubleSide,
+} );
+
+let material1 = new THREE.ShaderMaterial( {
+  uniforms: uniforms,
+  vertexShader: vs,
+  fragmentShader: fs1,
+  side: THREE.DoubleSide,
+} );
+
+const canvas = document.createElement('canvas');
+canvas.width = 512;
+canvas.height = 512;
+const ctx = canvas.getContext('2d');
+
+function loadsvg(url) {
+  fetch(url)
+  .then(r => r.text())
+  .then(text => {
+
+    xdoc = dom_parser.parseFromString(text, "text/xml");
+
+    let paths = xdoc.querySelectorAll('path');
+
+    let white = paths[0].outerHTML;
+    let black = paths[1].outerHTML;
+
+
+    let svg_string = ''; 
+
+    svg_string = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 512 512" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2">${white}</svg>`;
+
+    v0 = canvg.Canvg.fromString(ctx, svg_string);
+    v0.start();
+    im0 = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    svg_string = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 512 512" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2">${black}</svg>`;
+
+    v1 = canvg.Canvg.fromString(ctx, svg_string);
+    v1.start();
+    im1 = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    texture.image = im0;
+    texture.needsUpdate = true;
+
+    texture1.image = im1;
+    texture1.needsUpdate = true;
+
+    uniforms['colorTexture'].value = texture;
+    uniforms['strokeTexture'].value = texture1;
+
+    let obj = {
+      depths: [],
+      paths: [],
+      colors: [],
+      center: { x: 0, y: 0 },
+    };
+
+    obj.paths.push(paths[0].getAttribute('d'));
+    obj.colors.push(0xff00ee);
+    obj.depths.push(0.025);
+
+
+    let group = new THREE.Group();
+    addExtrudeObject( group, obj, material );
+
+    if(ext) {
+      scene.remove(ext);
+    }
+
+    ext = group;
+
+    scene.add(group);
+  });
+}
+
+function process_png(e) {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.drawImage(e.target, 0, 0);
+  let im1 = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+  let im0 = ctx.createImageData(ctx.canvas.width, ctx.canvas.height);
+  for(let i = 0; i < im1.data.length / 4; i++) {
+    let a = (255 - im1.data[4 * i + 3]) / 255;
+    let b = 255 * a | 0;
+    im0.data[4 * i + 0] = b;
+    im0.data[4 * i + 1] = b;
+    im0.data[4 * i + 2] = b;
+    im0.data[4 * i + 3] = 255;
+  }
+  ctx.putImageData(im0, 0, 0);
+  Potrace.loadImageFromUrl(ctx.canvas.toDataURL());
+  Potrace.process(function(){
+    let svg_string = Potrace.getSVG(1);
+    let xdoc = dom_parser.parseFromString(svg_string, "text/xml");
+    let paths = xdoc.querySelectorAll('path');
+    document.querySelector('#test').innerHTML = svg_string;
+
+    texture.image = im0;
+    texture.needsUpdate = true;
+
+    texture1.image = im1;
+    //texture1.wrapS = THREE.ClampToEdgeWrapping;
+    //texture1.wrapT = THREE.ClampToEdgeWrapping;
+    texture1.needsUpdate = true;
+
+    ctx.putImageData(im1, 0, 0);
+
+    /*
+    ctx.fillStyle = 'rgb(255, 0, 255)';
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.drawImage(ctx.canvas, 0, 0);
+    */
+
+    uniforms['colorTexture'].value = texture;
+    uniforms['strokeTexture'].value = texture1;
+
+    uniforms['base_color'].value = new THREE.Color(0.5+Math.random()*0.5,0.5+Math.random()*0.5,0.5+Math.random()*0.5);
+
+    let obj = {
+      depths: [],
+      paths: [],
+      colors: [],
+      center: { x: 0, y: 0 },
+    };
+
+    obj.paths.push(paths[0].getAttribute('d'));
+    obj.colors.push(0xff00ee);
+    obj.depths.push(0.025);
+
+
+    let group = new THREE.Group();
+    addExtrudeObject( group, obj, material1 );
+
+    if(ext) {
+      scene.remove(ext);
+    }
+
+    ext = group;
+
+    scene.add(group);
+  });
+}
+  
+function loadpng(url) {
+  let png_image = new Image();
+  png_image.src = url;
+  png_image.onload = process_png;
+}
+
+let frame_count = 0;
+function render() {
+  if(ext) {
+      ext.rotation.y = 0.2 * 3.141592 * Math.sin(frame_count / 60); 
+      //ext.rotation.y = msh.rotation.y;
+      let k = 0.5 + 0.5 * Math.sin(frame_count / 60 * 4);
+      ext.position.y = k*k*0.2 - 0.2;
+      uniforms['time'].value = frame_count / 60;
+    }
+  
+  renderer.render( scene, camera );
+  frame_count += 1;
+}
---
+++ play/evt/20_creatures/lib/svg_to_mesh.js
@@ -0,0 +1,281 @@
+function d3threeD(exports) {
+  const DEGS_TO_RADS = Math.PI / 180, UNIT_SIZE = 100;
+  const DIGIT_0 = 48, DIGIT_9 = 57, COMMA = 44, SPACE = 32, PERIOD = 46, MINUS = 45;
+
+  exports.transformSVGPath =
+  function transformSVGPath(pathStr) {
+    let path = new THREE.ShapePath();
+
+    let idx = 1, len = pathStr.length, activeCmd,
+      x = 0, y = 0, nx = 0, ny = 0, firstX = null, firstY = null,
+      x1 = 0, x2 = 0, y1 = 0, y2 = 0,
+      rx = 0, ry = 0, xar = 0, laf = 0, sf = 0, cx, cy;
+
+    function eatNum() {
+      let sidx, c, isFloat = false, s;
+      // eat delims
+      while (idx < len) {
+        c = pathStr.charCodeAt(idx);
+        if (c !== COMMA && c !== SPACE)
+          break;
+        idx++;
+      }
+      if (c === MINUS)
+        sidx = idx++;
+      else
+        sidx = idx;
+      // eat number
+      while (idx < len) {
+        c = pathStr.charCodeAt(idx);
+        if (DIGIT_0 <= c && c <= DIGIT_9) {
+          idx++;
+          continue;
+        }
+        else if (c === PERIOD) {
+          idx++;
+          isFloat = true;
+          continue;
+        }
+
+        s = pathStr.substring(sidx, idx);
+        return isFloat ? parseFloat(s) / 512 : parseInt(s) / 512;
+      }
+
+      s = pathStr.substring(sidx);
+      return isFloat ? parseFloat(s) / 512 : parseInt(s) / 512;
+    }
+
+    function nextIsNum() {
+      let c;
+      // do permanently eat any delims...
+      while (idx < len) {
+        c = pathStr.charCodeAt(idx);
+        if (c !== COMMA && c !== SPACE)
+          break;
+        idx++;
+      }
+      c = pathStr.charCodeAt(idx);
+      return (c === MINUS || (DIGIT_0 <= c && c <= DIGIT_9));
+    }
+
+    let canRepeat;
+    activeCmd = pathStr[0];
+    while (idx <= len) {
+      canRepeat = true;
+      switch (activeCmd) {
+        // moveto commands, become lineto's if repeated
+        case 'M':
+          x = eatNum();
+          y = eatNum();
+          path.moveTo(x, y);
+          activeCmd = 'L';
+          firstX = x;
+          firstY = y;
+          break;
+        case 'm':
+          x += eatNum();
+          y += eatNum();
+          path.moveTo(x, y);
+          activeCmd = 'l';
+          firstX = x;
+          firstY = y;
+          break;
+        case 'Z':
+        case 'z':
+          canRepeat = false;
+          if (x !== firstX || y !== firstY)
+            path.lineTo(firstX, firstY);
+          break;
+        // - lines!
+        case 'L':
+        case 'H':
+        case 'V':
+          nx = (activeCmd === 'V') ? x : eatNum();
+          ny = (activeCmd === 'H') ? y : eatNum();
+          path.lineTo(nx, ny);
+          x = nx;
+          y = ny;
+          break;
+        case 'l':
+        case 'h':
+        case 'v':
+          nx = (activeCmd === 'v') ? x : (x + eatNum());
+          ny = (activeCmd === 'h') ? y : (y + eatNum());
+          path.lineTo(nx, ny);
+          x = nx;
+          y = ny;
+          break;
+        // - cubic bezier
+        case 'C':
+          x1 = eatNum(); y1 = eatNum();
+        case 'S':
+          if (activeCmd === 'S') {
+            x1 = 2 * x - x2; y1 = 2 * y - y2;
+          }
+          x2 = eatNum();
+          y2 = eatNum();
+          nx = eatNum();
+          ny = eatNum();
+          path.bezierCurveTo(x1, y1, x2, y2, nx, ny);
+          x = nx; y = ny;
+          break;
+        case 'c':
+          x1 = x + eatNum();
+          y1 = y + eatNum();
+        case 's':
+          if (activeCmd === 's') {
+            x1 = 2 * x - x2;
+            y1 = 2 * y - y2;
+          }
+          x2 = x + eatNum();
+          y2 = y + eatNum();
+          nx = x + eatNum();
+          ny = y + eatNum();
+          path.bezierCurveTo(x1, y1, x2, y2, nx, ny);
+          x = nx; y = ny;
+          break;
+        // - quadratic bezier
+        case 'Q':
+          x1 = eatNum(); y1 = eatNum();
+        case 'T':
+          if (activeCmd === 'T') {
+            x1 = 2 * x - x1;
+            y1 = 2 * y - y1;
+          }
+          nx = eatNum();
+          ny = eatNum();
+          path.quadraticCurveTo(x1, y1, nx, ny);
+          x = nx;
+          y = ny;
+          break;
+        case 'q':
+          x1 = x + eatNum();
+          y1 = y + eatNum();
+        case 't':
+          if (activeCmd === 't') {
+            x1 = 2 * x - x1;
+            y1 = 2 * y - y1;
+          }
+          nx = x + eatNum();
+          ny = y + eatNum();
+          path.quadraticCurveTo(x1, y1, nx, ny);
+          x = nx; y = ny;
+          break;
+        // - elliptical arc
+        case 'A':
+          rx = eatNum();
+          ry = eatNum();
+          xar = eatNum() * DEGS_TO_RADS;
+          laf = eatNum();
+          sf = eatNum();
+          nx = eatNum();
+          ny = eatNum();
+          if (rx !== ry) {
+            console.warn("Forcing elliptical arc to be a circular one :(",
+              rx, ry);
+          }
+          // SVG implementation notes does all the math for us! woo!
+          // http://www.w3.org/TR/SVG/implnote.html#ArcImplementationNotes
+          // step1, using x1 as x1'
+          x1 = Math.cos(xar) * (x - nx) / 2 + Math.sin(xar) * (y - ny) / 2;
+          y1 = -Math.sin(xar) * (x - nx) / 2 + Math.cos(xar) * (y - ny) / 2;
+          // step 2, using x2 as cx'
+          let norm = Math.sqrt(
+             (rx*rx * ry*ry - rx*rx * y1*y1 - ry*ry * x1*x1) /
+             (rx*rx * y1*y1 + ry*ry * x1*x1));
+          if (laf === sf)
+            norm = -norm;
+          x2 = norm * rx * y1 / ry;
+          y2 = norm * -ry * x1 / rx;
+          // step 3
+          cx = Math.cos(xar) * x2 - Math.sin(xar) * y2 + (x + nx) / 2;
+          cy = Math.sin(xar) * x2 + Math.cos(xar) * y2 + (y + ny) / 2;
+
+          let u = new THREE.Vector2(1, 0),
+            v = new THREE.Vector2((x1 - x2) / rx,
+                                  (y1 - y2) / ry);
+          let startAng = Math.acos(u.dot(v) / u.length() / v.length());
+          if (u.x * v.y - u.y * v.x < 0)
+            startAng = -startAng;
+
+          // we can reuse 'v' from start angle as our 'u' for delta angle
+          u.x = (-x1 - x2) / rx;
+          u.y = (-y1 - y2) / ry;
+
+          let deltaAng = Math.acos(v.dot(u) / v.length() / u.length());
+          // This normalization ends up making our curves fail to triangulate...
+          if (v.x * u.y - v.y * u.x < 0)
+            deltaAng = -deltaAng;
+          if (!sf && deltaAng > 0)
+            deltaAng -= Math.PI * 2;
+          if (sf && deltaAng < 0)
+            deltaAng += Math.PI * 2;
+
+          path.absarc(cx, cy, rx, startAng, startAng + deltaAng, sf);
+          x = nx;
+          y = ny;
+          break;
+        default:
+          throw new Error("weird path command: " + activeCmd);
+      }
+      // just reissue the command
+      if (canRepeat && nextIsNum())
+        continue;
+      activeCmd = pathStr[idx++];
+    }
+
+    return path;
+  }
+}
+
+var $d3g = {};
+d3threeD($d3g);
+
+function addExtrudeObject( group, svgObject, mat ) {
+  let i,j, len, len1;
+  let path, mesh, color, material, depth, simpleShapes, simpleShape, shape3d, x, toAdd, results = [];
+  let thePaths = svgObject.paths;
+  let theDepths = svgObject.depths;
+  let theColors = svgObject.colors;
+  let theCenter = svgObject.center;
+
+  color = new THREE.Color( theColors[theColors.length * Math.random() | 0] );
+  
+  /*
+  material = new THREE.MeshLambertMaterial({
+    color: color,
+    //emissive: color,
+  });
+  */
+  
+  len = thePaths.length;
+  //console.log(len);
+  for (i = 0; i < len; ++i) {
+    path = $d3g.transformSVGPath( thePaths[i] );
+
+    depth = theDepths[i];
+    simpleShapes = path.toShapes(false); // CCW true
+    len1 = simpleShapes.length;
+    for (j = 0; j < len1; ++j) {
+      simpleShape = simpleShapes[j];
+      let extrudeSettings = {
+        depth: depth,
+        bevelEnabled: false,
+      };
+
+      shape3d = new THREE.ExtrudeGeometry( simpleShape, extrudeSettings );      
+
+      mesh = new THREE.Mesh(shape3d, mat);
+
+      let pivot = new THREE.Group();
+      pivot.rotation.x = Math.PI;
+      pivot.translateX( - 0.5);
+      pivot.translateY( - 0.5);
+      pivot.updateMatrix();
+
+      mesh.geometry.applyMatrix4( pivot.matrix );
+
+      group.add(mesh);
+    }
+  }
+}
