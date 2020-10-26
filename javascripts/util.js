47e7df68eee9316f68fe7ce117fcc49b9cb54dfe
somi <somi@somi.local>
2020-10-26 09:48:49 +0900

    update

--- javascripts/util.js
+++ javascripts/util.js
@@ -1,7 +1,3 @@
-function mix(a, b, t) {
-  return (1 - t) * a + t * b;
-}
-
 function pointInQuadraticBezier(a, b, c, t) {
   let ab = [
       mix(a[0], b[0], t),
@@ -69,11 +65,26 @@ function buildRandomCurve(start, end, n, steps = 50, radius = 200) {
 
 function rand(min, max) {
   return min + (max - min) * Math.random();
-};
+}
 
 function randi(min, max) {
   return Math.floor(rand(min, max));
-};
+}
+
+function shuffle(array) {
+  let n = array.length, t, i;
+  while (n) {
+    i = Math.random() * n-- | 0; // 0 ≤ i < n
+    t = array[n];
+    array[n] = array[i];
+    array[i] = t;
+  }
+  return array;
+}
+
+function mix(a, b, t) {
+  return (1 - t) * a + t * b;
+}
 
 function hue_to_rgb(m1, m2, h) {
   if(h < 0) h = h + 1;
@@ -82,13 +93,13 @@ function hue_to_rgb(m1, m2, h) {
   if(h * 2 < 1) return m2;
   if(h * 3 < 2) return m1 + (m2 - m1) * (2 / 3 - h) * 6;
   return m1;
-};
+}
 
 function hsl_to_rgb(h, s, l) {
   h = h / 360;
   s = s / 100;
   l = l / 100;
-  var r, g, b, m1, m2;
+  let r, g, b, m1, m2;
 
   if(l <= 0.5) {
     m2 = l * (s + 1);
@@ -101,18 +112,18 @@ function hsl_to_rgb(h, s, l) {
   g = hue_to_rgb(m1, m2, h);
   b = hue_to_rgb(m1, m2, h - 1 / 3);
   return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
-};
+}
 
 function rgb_to_hsl(r, g, b) {
   r /= 255, g /= 255, b /= 255;
-  var max = Math.max(r, g, b), min = Math.min(r, g, b);
-  var h, s, l = (max + min) / 2;
+  let max = Math.max(r, g, b), min = Math.min(r, g, b);
+  let h, s, l = (max + min) / 2;
 
   if(max == min){
     h = s = 0;
   }
   else {
-    var d = max - min;
+    let d = max - min;
     s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
     switch(max){
       case r: h = (g - b) / d + (g < b ? 6 : 0); break;
@@ -122,15 +133,15 @@ function rgb_to_hsl(r, g, b) {
     h /= 6;
   }
   return [Math.floor(h * 360), Math.floor(s * 100), Math.floor(l * 100)];
-};
+}
 
 function length(p) {
   return Math.sqrt(p.x*p.x + p.y*p.y);
 }
 
 function distance(p0, p1) {
-  var dx = p1.x - p0.x;
-  var dy = p1.y - p0.y;
+  let dx = p1.x - p0.x;
+  let dy = p1.y - p0.y;
   return Math.sqrt(dx*dx + dy*dy);  
 }
 
@@ -148,7 +159,7 @@ function smoothstep(a, b, x) {
 }
 
 function motor(min, max, time) {
-  var t = 0.5 + 0.5 * Math.sin(time);
+  let t = 0.5 + 0.5 * Math.sin(time);
   return mix(min, max, t);
 }
 
@@ -158,13 +169,13 @@ function rotate_from_origin(origin, target, r, angle) {
 }
 
 function preserve(p0, p1, len) {
-  var dx = p1.x - p0.x;
-  var dy = p1.y - p0.y;
-  var d = Math.sqrt(dx*dx + dy*dy);
+  let dx = p1.x - p0.x;
+  let dy = p1.y - p0.y;
+  let d = Math.sqrt(dx*dx + dy*dy);
   if(d > 0) {
-    var ux = dx / d;
-    var uy = dy / d;
-    var dl = len - d;
+    let ux = dx / d;
+    let uy = dy / d;
+    let dl = len - d;
     p0.x += -dl * ux * 0.5;
     p0.y += -dl * uy * 0.5;
     p1.x += dl * ux * 0.5;
