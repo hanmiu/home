13fa6a6359a4c02f748d3163c14676eeb3858cd4
somi <somi@somi.local>
2020-11-04 10:22:10 +0900

    update

--- javascripts/hanmi_playground.js
+++ javascripts/hanmi_playground.js
@@ -8,7 +8,7 @@ html['playground'] = `
   <a class="no-underline" href="https://www.hanmiu.cc/play/glyph/hangul-slime.html">
     <img class="full" src="./play/glyph/hangul-slime.jpg">
   </a>
-  <div class="subtle">모음과 자음 사이에 인력이 작용합니다. 빈 공간을 터치(클릭)하면 그 쪽으로 한글 자모가 모입니다.<br>데스크탑+모바일 지원합니다. 위 링크 또는 이미지를 클릭하여 어린이와 함께 놀아보세요.</div>
+  <div class="subtle">모음과 자음 사이에 인력이 작용합니다. 빈 공간을 터치(클릭)하면 그 쪽으로 한글 자모가 모입니다. 데스크탑+모바일 지원합니다. 위 링크 또는 이미지를 클릭하여 어린이와 함께 놀아보세요.</div>
   <br>
 
   <div class="subtle"><a href="https://www.hanmiu.cc/play/glyph/hangul-slime.html" target="_blank"><strong>
@@ -17,10 +17,28 @@ html['playground'] = `
   <a class="no-underline" href="https://www.hanmiu.cc/play/glyph/mmj.html">
     <img class="full" src="./play/glyph/mmj.jpg">
   </a>
-  <div class="subtle">2018학년도 <a href="https://www.youtube.com/watch?v=x25lngyKUAU" target="_blank">몸문자</a> 놀이를 지원하기 위해 만들었습니다. 어린이들의 호기심과 제안을 따라가며 문자 체계를 고안해 갔던 이야기입니다. 자세한 내용은 <a href="https://www.youtube.com/watch?v=x25lngyKUAU" target="_blank">몸문자</a> 영상을 살펴봐 주세요.<br>데스크탑+모바일 지원합니다.</div>
+  <div class="subtle"><a href="https://www.youtube.com/watch?v=x25lngyKUAU" target="_blank">몸문자</a> 놀이를 지원하기 위해 만들었습니다. 어린이들의 호기심과 제안을 따라가며 문자 체계를 고안해 갔던 이야기입니다. 자세한 내용은 <a href="https://www.youtube.com/watch?v=x25lngyKUAU" target="_blank">몸문자</a> 영상을 살펴봐 주세요. (데스크탑+모바일 지원)</div>
   <br>
 
-  <div class="subtle">한미유치원의 교실 놀이를 지원하기 위해 만들었던 다양한 작업을 소개하며 건강한 미디어 활용을 다룹니다. 계속 업데이트 됩니다.</div>
+  <div class="subtle"><a href="https://www.hanmiu.cc/play/glyph/snowflower.html" target="_blank"><strong>
+  눈꽃
+  </strong></div>
+  <a class="no-underline" href="https://www.hanmiu.cc/play/glyph/snowflower.html">
+    <img class="full" src="./play/glyph/snowflower.jpg">
+  </a>
+  <div class="subtle"><a href="https://youtu.be/AmFalsFzkYA?t=948" target="_blank">글자 디자인</a> 놀이를 지원하기 위해 만들었습니다. 만4세의 한글에 관한 탐색을 지원하기 위한 여러 접근 중에 하나로 활용되었습니다. 자세한 내용은 <a href="https://youtu.be/AmFalsFzkYA?t=948" target="_blank">글자 디자인</a> 영상을 살펴봐 주세요. (데스크탑+모바일 지원)</div>
+  <br>
+
+  <div class="subtle"><a href="https://www.hanmiu.cc/play/glyph/drawing.html" target="_blank"><strong>
+  글자 드로잉
+  </strong></div>
+  <a class="no-underline" href="https://www.hanmiu.cc/play/glyph/drawing.html">
+    <img class="full" src="./play/glyph/drawing.jpg">
+  </a>
+  <div class="subtle"><a href="https://youtu.be/AmFalsFzkYA?t=948" target="_blank">글자 디자인</a> 놀이를 지원하기 위해 만들었습니다. <a href="https://docs.google.com/document/d/1yZu8Pt3nYTMR1Zs17U5BEG83POhI7FT3OGwvEHcniME/view" target="_blank">'일상 속의 호기심 탐구 창의성'</a>이란 흐름과 연결되어 있던 교실의 다양한 접근이었습니다. 더 자세한 내용은 <a href="https://youtu.be/AmFalsFzkYA?t=948" target="_blank">글자 디자인</a> 영상과 <a href="https://docs.google.com/document/d/1yZu8Pt3nYTMR1Zs17U5BEG83POhI7FT3OGwvEHcniME/view" target="_blank">'일상 속의 호기심 탐구 창의성'</a>을 살펴봐 주세요. (데스크탑+모바일 지원)</div>
+  <br>
+
+  <div class="subtle">먼저 한글(또는 문자)에 관한 어린이의 호기심을 지원하기 위해 만들었던 작업들을 소개드리고 있습니다. 그 외에도 수나 음악과 연결되어 있는 한미유치원의 교실 놀이를 지원하기 위해 만들었던 다양한 작업을 소개하며 건강한 미디어 활용을 다를 계획입니다. (계속 업데이트 됩니다.)</div>
   <br>
 
   <div>더 살펴보기</div>
