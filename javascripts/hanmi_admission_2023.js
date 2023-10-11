c37e9d92d27ab7c7afcd9ff04b85fcebf5cc2537
seung joon choi <erucipe@gmail.com>
2023-10-12 01:30:21 +0900

    Create hanmi_admission_2023.js

---
+++ javascripts/hanmi_admission_2023.js
@@ -0,0 +1,245 @@
+html['admission'] = `
+<div>2023학년도 한미유치원 입학 안내</div>
+<br>
+<div class="subtle">
+<!--
+<a href="https://drive.google.com/file/d/10_UtFlgPmo0OVc-u8V9TfLvDPL0_JNoz/view?usp=sharing" target="_blank"><strong>2023학년도 설명회 자료 및 모집 요강</strong></a>  (← 클릭)<br>-->
+<a href="https://instagram.com/hanmiucw" target="_blank">한미유치원 인스타그램</a>을 통해 환경 사진과 연령별 간추린 교실 이야기를 소개하고 있습니다.<br>
+</div>
+<br>
+
+<div class="subtle">
+등록하신 가정에서는 아래의 링크에서 입학원서를 작성하실 수 있습니다.<br>
+<strong><a href="https://forms.gle/MoadBAdVCpyqsDQ57" target="_blank">입학원서 작성</a></strong>
+</div>
+<br>
+
+<!--
+<div class="subtle">
+2023학년도 한미유치원 입학에 관심가져 주셔서 감사합니다.<br><br>
+추가 모집에 관한 문의는 전화 <a href="tel:031-975-6567">031-975-6567</a>, <a href="tel:031-976-6567">031-976-6567</a>로 부탁드립니다.  
+</div>
+<br>
+
+<div>2023학년도 입학 원서</div>
+<br>
+<div class="subtle">
+입학 확정 후 <a href="https://forms.gle/B6S3iemZqbknvAbH8" target="_blank">2022학년도 입학원서</a>를 클릭하여 온라인으로 작성해 주시기 바랍니다.  
+</div>
+<br>
+
+모집 대상
+<div class="subtle">
+<ul class="flower">
+    <li>2019년생</li>
+    <li>2018년생</li>
+    <li>2017년생</li>
+  </ul>
+  설명회 기간 다뤄졌던 내용은 <a href="https://bit.ly/3qjWFzD" target="_blank">대면 설명회 안내</a>에서 자세하게 살펴볼 수 있습니다.<br>
+  2021학년도 현재 <a href="https://bit.ly/3oXJnXo" target="_blank">만3세반</a>, <a href="https://bit.ly/3DUzLTB" target="_blank">만4세반</a>, <a href="https://bit.ly/3pcpDzl" target="_blank">만5세반</a>, <a href="https://bit.ly/3pdcaY9" target="_blank">전시장</a>의 공간과 교육을 사진과 간략한 글로 소개합니다.
+  
+</div>
+-->
+
+
+
+모집대상
+<div class="subtle">
+  <ul class="flower">
+    <!--
+    <li>2019년생 (만3세) 54명</li>
+    <li>2018년생 (만4세) 37명</li>
+    <li>2017년생 (만5세) 12명</li>
+    -->
+    <li>2019년생 (만3세) O명</li>
+    <li>2018년생 (만4세) O명</li>
+    <li>2017년생 (만5세) O명</li>
+  </ul>
+</div>
+
+<div class="subtle">
+추가 모집에 관한 문의는 전화 <a href="tel:031-975-6567">031-975-6567</a>, <a href="tel:031-976-6567">031-976-6567</a>로 부탁드립니다.  
+</div>
+<br>
+
+<strong>교육 설명회</strong>
+<br><br>
+<div class="subtle">
+  <a href="https://drive.google.com/file/d/10_UtFlgPmo0OVc-u8V9TfLvDPL0_JNoz/view?usp=sharing" target="_blank"><strong>2023학년도 설명회 자료 및 모집 요강</strong></a>  (← 클릭)
+  <br><br>
+  <a href="https://bit.ly/3qjWFzD" target="_blank">2022학년도 설명회 안내</a>에서 작년에 다뤘던 내용도 살펴보실 수 있습니다.
+</div>
+
+<!--
+<strong>교육 설명회</strong>
+<div class="subtle">
+  <ul class="leaf">
+    <li>우선 모집: 10/26(수) 16시 30분, 10/27(목) 16시 30분 (종료)</li>
+    <li>일반 모집: 11/14(월) 16시 30분, <a href="https://forms.gle/1kwVNaahWW7QWffr8" target="_blank"><strong>설명회 및 시설 참관 신청</strong></a>  (← 클릭) </li>
+  </ul>
+  <a href="https://drive.google.com/file/d/10_UtFlgPmo0OVc-u8V9TfLvDPL0_JNoz/view?usp=sharing" target="_blank"><strong>2023학년도 설명회 자료 및 모집 요강</strong></a>  (← 클릭)
+  <br><br>
+  <a href="https://bit.ly/3qjWFzD" target="_blank">2022학년도 설명회 안내</a>에서 작년에 다뤘던 내용도 살펴보실 수 있습니다.
+  <br><br>
+
+<strong>시설 참관</strong>
+<div class="subtle">
+  <ul class="leaf">
+    <li>10월 24일 (월) ~ 10월 28일 (금) 16시 30분 ~ 18시</li>
+    <li>11월 14일 설명회 이후에 이어서 진행합니다. <a href="https://forms.gle/1kwVNaahWW7QWffr8" target="_blank"><strong>설명회 및 시설 참관 신청</strong></a></li>
+  </ul>
+  이 기간 동안 한미유치원을 방문하여 시설을 둘러보고 소개를 받으실 수 있습니다.
+</div>
+<br>
+
+<div class="subtle">
+<a href="https://www.go-firstschool.go.kr" target="_blank">처음학교로</a>의 일정에 맞춰 계속 이 페이지의 내용을 업데이트할 예정입니다.
+  <ul class="flower">
+    <li>우선 모집: 10월 31일 (월) ~ 11월 2일 (수)</li>
+    <li>일반 모집: 11월 16일 (수) ~ 11월 18일 (금)</li>
+  </ul>
+</div>
+-->
+
+<img src="./images/2022/2023_vintro_1.svg" style="width: 100%">
+<br>
+<img src="./images/2022/2023_vintro_2.svg" style="width: 100%">
+<br>
+<img src="./images/2022/2023_vintro_3.svg" style="width: 100%">
+<br>
+<img src="./images/2022/2023_vintro_4.svg" style="width: 100%">
+<br>
+<img src="./images/2022/2023_vintro_5.svg" style="width: 100%">
+<br>
+<img src="./images/2022/2023_vintro_6.svg" style="width: 100%">
+<br><br>
+
+<div>그 밖의 정보</div>
+<br>
+<div class="subtle">
+<strong><a href="https://drive.google.com/file/d/10_UtFlgPmo0OVc-u8V9TfLvDPL0_JNoz/view?usp=sharing" target="_blank">설명회 자료의 유아모집 요강</a></strong>에서 관련 제출서류, 방과후과정반, 특성화활동, 통학차량 및 등하원 시간, 교육비 등의 내용을 확인할 수 있습니다. <strong><a href="https://instagram.com/hanmiucw" target="_blank">한미유치원 인스타그램</a></strong>을 통해 2022학년도 현재 환경 사진과 간추린 교실 이야기 등 설명회 자료에 넣지 못한 다양한 이미지를 공유하고 있습니다.<br><br>
+  <ul class="musical">
+    <li>2023학년도 <strong>방과후 과정 참여</strong>가 필요한 맞벌이 가정과 돌봄이 필요한 가정은 <strong>누구나 신청</strong>할 수 있습니다.</li>
+  </ul>
+  <br>
+</div>
+<div>
+
+<div class="subtle">설명회 전후에 이 홈페이지의 내용을 통해 한미유치원의 교육에 관한 다양한 자료를 살펴보실 수 있습니다. 교실 공간과 환경은 <a href="/?page=hanmi" target="_blank">&lt;한미유치원의 역사·문화·환경&gt;</a> 페이지를, 어린이들이 사용하는 놀잇감은 <a href="/?page=about_play" target="_blank">&lt;놀이&gt;</a> 페이지를 살펴봐 주세요.</div>
+<br>
+
+<!--
+<div class="iframe-container">
+  <iframe class="video" src="https://www.youtube.com/embed/YJaZa7rP5HA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+<div style="margin-bottom: 0.5em; text-align: right; font-size: 0.5em; line-height: 1.5rem">2015 진달래반, 적응</div>
+<div class="subtle">어린이들이 학년 초에 어떻게 새로운 환경에 적응하는지에 관한 이야기입니다.</div>
+<br>
+
+<div class="iframe-container">
+  <iframe class="video" src="https://www.youtube.com/embed/T7XMDoV7WUU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+<div style="margin-bottom: 0.5em; text-align: right; font-size: 0.5em; line-height: 1.5rem">2015 개나리반, 한미유치원 마을</div>
+<div class="subtle">어린이들이 입학 후 1년의 시간을 걸쳐 놀이를 통해 어떤 경험을 하게 되는지를 보여주는 영상 중 하나입니다. <a href="https://www.youtube.com/c/hanmiu/videos" target="_blank">영상으로 보는 한미유치원의 놀이</a> 중 꽃 이름의 반이 만3세, 나무 이름의 반이 만4세, 새 이름의 반이 만5세 이야기입니다.</div>
+<br>
+-->
+
+<br>
+
+<div class="subtle">
+입학에 관해 궁금한 사항은 전화로 문의해 주시면 상세히 답변 드리겠습니다.<br><br>
+<a href="tel:031-975-6567">031-975-6567</a>, <a href="tel:031-976-6567">031-976-6567</a>
+</div>
+<br>
+
+<div>더 살펴보기</div>
+<div class="subtle">
+  <ul class="leaf">
+    <li><a href="https://trello.com/b/sFYg60Ly" target="_blank">한미유치원의 1년</a></li>
+    <li><a href="http://www.youtube.com/watch?v=JJtlmzplcWA" target="_blank">한미유치원 하루 일과 (영상)</a></li>
+    <li><a href="/?page=hanmi">한미유치원의 역사·문화·환경</a> →</li>
+    <li><a href="/?page=thought">한미유치원의 교육에 관한 생각</a> →</li>
+    <li><a href="/?page=about_play">영상으로 보는 풍요롭고 재밌는 한미유치원의 놀이</a> →</li>
+    <li><a href="/?page=playground">한미유치원의 디지털 놀이터 - 어린이들의 놀이를 지원하기 위한 건강한 미디어</a> →</li>
+  </ul>
+</div>
+
+<img style="display: block; height: 100px; margin: auto;" src="./images/logotype/hanmi_runner.svg">
+`;
+
+class HanmiAdmission {
+  constructor() {
+    let d = d_child;
+    
+    this.path = new Path2D(d);
+    this.frame_count = 0;
+    this.x = 400 + Math.random() * 400;
+    this.y = 100 + (1-Math.random()*2) * 50;
+    
+    this.wait = 120;
+    
+    this.bipeds = [];
+    for(let i = 0; i < biped_classes.length; i++) {
+      this.bipeds.push(new biped_classes[i]);
+    }
+    let bc = biped_classes_for_shuffle.shift();
+    if(biped_classes_for_shuffle.length === 0) {
+      biped_classes_for_shuffle = biped_classes.map(x => x);
+    }
+    this.biped = this.bipeds[this.bipeds.map(x => x.constructor).indexOf(bc)];
+    this.biped_x = -100;
+    for(let i = 0; i < this.bipeds.length; i++) {
+      this.bipeds[i].x = this.biped_x;
+    }
+  }
+  
+  update(c) {
+    let dx = hanmi_base.x - this.x;
+    let dy = hanmi_base.y - this.y;
+    let d = Math.sqrt(dx*dx + dy*dy) + 1e-6;
+    if(d > 100) {
+      let ux = dx / d;
+      let uy = dy / d;
+      this.x += ux * 10; 
+      this.y += uy * 10;  
+    }
+    else {
+      this.x += dx * 0.25; 
+      this.y += dy * 0.25; 
+    }
+    
+    if(this.wait > 0) {
+      this.wait -= 1;
+    }
+    
+    if(c === current_ctx) {
+      this.frame_count += 1;
+      this.biped_x += 1;
+      this.biped.update(this.frame_count / 60);
+      this.biped.x = this.biped_x;
+      if(this.biped.x > c.canvas.width + 200) {
+        let bc = biped_classes_for_shuffle.shift();
+        if(biped_classes_for_shuffle.length === 0) {
+          biped_classes_for_shuffle = biped_classes.map(x => x);
+        }
+        this.biped = this.bipeds[this.bipeds.map(x => x.constructor).indexOf(bc)];
+        this.biped_x = -100;
+        this.biped.reform();
+      }  
+    }
+  }
+  
+  draw(c) {
+    c.clearRect(0, 0, c.canvas.width, c.canvas.height);
+    
+    c.save();
+    c.translate(this.x, this.y);
+    c.scale(hanmi_base.scale, hanmi_base.scale);
+    c.translate(-256, -256);
+    c.fillStyle = 'hsla(220, 100%, 50%, 0.5)';
+    c.fill(this.path);
+    c.restore();
+    
+    this.biped.draw(c, this);
+  }
+}
