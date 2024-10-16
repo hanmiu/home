274e137ff9443984498fc6c1933f71cc2ad18197
seung joon choi <erucipe@gmail.com>
2024-10-17 05:55:25 +0900

    update 2025 입학

--- javascripts/hanmi_admission_2024.js
+++ javascripts/hanmi_admission_2024.js
@@ -1,11 +1,33 @@
 html['admission'] = `
-<div>2025학년도 한미유치원 입학 안내</div>
+<div>2024학년도 한미유치원 입학 안내</div>
 <br>
 <div class="subtle">
-
+<!--
+<a href="https://drive.google.com/file/d/10_UtFlgPmo0OVc-u8V9TfLvDPL0_JNoz/view?usp=sharing" target="_blank"><strong>2023학년도 설명회 자료 및 모집 요강</strong></a>  (← 클릭)<br>-->
 <a href="https://instagram.com/hanmiucw" target="_blank">한미유치원 인스타그램</a>을 통해 환경 사진과 연령별 간추린 교실 이야기를 소개하고 있습니다.<br>
 </div>
 <br>
+
+<!--
+<div class="subtle">
+2024학년도 한미유치원 입학에 관심가져 주셔서 감사합니다.<br><br>
+모집에 관한 문의는 전화 <a href="tel:031-975-6567">031-975-6567</a>, <a href="tel:031-976-6567">031-976-6567</a>로 부탁드립니다.  
+</div>
+<br>
+
+모집 대상
+<div class="subtle">
+<ul class="flower">
+    <li>2020년생</li>
+    <li>2019년생</li>
+    <li>2018년생</li>
+  </ul>
+  설명회 기간 다뤄졌던 내용은 <a href="https://bit.ly/3qjWFzD" target="_blank">대면 설명회 안내</a>에서 자세하게 살펴볼 수 있습니다.<br>
+  2021학년도 현재 <a href="https://bit.ly/3oXJnXo" target="_blank">만3세반</a>, <a href="https://bit.ly/3DUzLTB" target="_blank">만4세반</a>, <a href="https://bit.ly/3pcpDzl" target="_blank">만5세반</a>, <a href="https://bit.ly/3pdcaY9" target="_blank">전시장</a>의 공간과 교육을 사진과 간략한 글로 소개합니다.
+  
+</div>
+-->
+
 
 
 모집대상
@@ -16,9 +38,9 @@ html['admission'] = `
     <li>2018년생 (만4세) 37명</li>
     <li>2017년생 (만5세) 12명</li>
     -->
-    <li>2021년생 (3세)</li>
-    <li>2020년생 (4세)</li>
-    <li>2019년생 (5세)</li>
+    <li>2020년생 (3세) 약간명</li>
+    <li>2019년생 (4세) 대기 가능</li>
+    <li>2018년생 (5세) 약간명</li>
   </ul>
 </div>
 
@@ -27,31 +49,29 @@ html['admission'] = `
 </div>
 <br>
 
-<!--
-<strong>2025학년도 입학 원서</strong>
+<strong>2024학년도 입학 원서</strong>
 <br><br>
 <div class="subtle">
 입학 확정 후 <a href="https://forms.gle/Kfa7BndVgcBPPWCN8" target="_blank">2024학년도 입학원서</a>를 클릭하여 온라인으로 작성해 주시기 바랍니다.  
 </div>
 <br>
--->
 
 
-<div>교육 설명회 2025</div>
+<div>교육 설명회 2024</div>
 <div class="subtle">
   <ul class="leaf">
-    <li>우선 모집: 10/31(목), 11/01(금) 16시 30분</li>
-    <!--<li>일반 모집: 11/14(화) 16시 30분</li>-->
+    <li>우선 모집: 10/31(화), 11/01(수) 16시 30분</li>
+    <li>일반 모집: 11/14(화) 16시 30분</li>
     <!--<li>일반 모집: 11/14(월) 16시 30분, <a href="https://forms.gle/1kwVNaahWW7QWffr8" target="_blank"><strong>설명회 및 시설 참관 신청</strong></a>  (← 클릭) </li>-->
   </ul>
   <!--<a href="https://drive.google.com/file/d/10_UtFlgPmo0OVc-u8V9TfLvDPL0_JNoz/view?usp=sharing" target="_blank"><strong>2023학년도 설명회 자료 및 모집 요강</strong></a>  (← 클릭)-->
-  <a href="https://drive.google.com/file/d/1ApPpTBmcWLjJst7RqSw0CMZY2v3GrbAU/view?usp=sharing" target="_blank">작년 설명회 자료</a>에서 한미유치원의 교육과 환경에 관한 이야기를 살펴보실 수 있습니다.<br> <a href="https://bit.ly/408ZT96" target"_blank">디지털 놀이터와 연결되어 있는 교실 놀이</a>에도 다양한 사례가 있으니 참고해 주세요.
+  <a href="https://drive.google.com/file/d/1ApPpTBmcWLjJst7RqSw0CMZY2v3GrbAU/view?usp=sharing" target="_blank">2024학년도 설명회 자료 및 모집 요강</a>에서 한미유치원의 교육과 환경에 관한 이야기를 살펴보실 수 있습니다. <a href="https://bit.ly/408ZT96" target"_blank">디지털 놀이터와 연결되어 있는 교실 놀이</a>에도 다양한 사례가 있으니 참고해 주세요.
   <br><br><br>
 
 <div>시설 참관</div>
 <div class="subtle">
   <ul class="leaf">
-    <li>10월 31일 (목) ~ 11월 01일 (금) 16시 30분 ~ 18시</li>
+    <li>10월 30일 (월) ~ 11월 03일 (금) 16시 30분 ~ 18시</li>
     <!--<li>11월 14일 설명회 이후에 이어서 진행합니다. <a href="https://forms.gle/1kwVNaahWW7QWffr8" target="_blank"><strong>설명회 및 시설 참관 신청</strong></a></li>-->
   </ul>
   이 기간 동안 한미유치원을 방문하여 시설을 둘러보고 소개를 받으실 수 있습니다.
@@ -61,13 +81,54 @@ html['admission'] = `
 <div class="subtle">
 <a href="https://www.go-firstschool.go.kr" target="_blank">처음학교로</a>의 일정에 맞춰 계속 이 페이지의 내용을 업데이트할 예정입니다.
   <ul class="flower">
-    <li>우선 모집: 11월 01일 (금) ~ 11월 05일 (화)</li>
-    <li>일반 모집: 11월 19일 (화) ~ 11월 22일 (금)</li>
+    <li>우선 모집: 11월 01일 (수) ~ 11월 03일 (금)</li>
+    <li>일반 모집: 11월 17일 (금) ~ 11월 21일 (화)</li>
   </ul>
 </div>
 
 
-<img src="./images/2024/2025_vintro_1.svg" style="width: 100%">
+<!--
+<strong>교육 설명회</strong>
+<br><br>
+<div class="subtle">
+  <a href="https://drive.google.com/file/d/10_UtFlgPmo0OVc-u8V9TfLvDPL0_JNoz/view?usp=sharing" target="_blank"><strong>2023학년도 설명회 자료 및 모집 요강</strong></a>  (← 클릭)
+  <br><br>
+  <a href="https://bit.ly/3qjWFzD" target="_blank">2022학년도 설명회 안내</a>에서 작년에 다뤘던 내용도 살펴보실 수 있습니다.
+</div>
+-->
+
+<!--
+<strong>교육 설명회 2023</strong>
+<div class="subtle">
+  <ul class="leaf">
+    <li>우선 모집: 10/26(수) 16시 30분, 10/27(목) 16시 30분 (종료)</li>
+    <li>일반 모집: 11/14(월) 16시 30분, <a href="https://forms.gle/1kwVNaahWW7QWffr8" target="_blank"><strong>설명회 및 시설 참관 신청</strong></a>  (← 클릭) </li>
+  </ul>
+  <a href="https://drive.google.com/file/d/10_UtFlgPmo0OVc-u8V9TfLvDPL0_JNoz/view?usp=sharing" target="_blank"><strong>2023학년도 설명회 자료 및 모집 요강</strong></a>  (← 클릭)
+  <br><br>
+  <a href="https://bit.ly/3qjWFzD" target="_blank">2022학년도 설명회 안내</a>에서 작년에 다뤘던 내용도 살펴보실 수 있습니다.
+  <br><br>
+
+<strong>시설 참관</strong>
+<div class="subtle">
+  <ul class="leaf">
+    <li>10월 24일 (월) ~ 10월 28일 (금) 16시 30분 ~ 18시</li>
+    <li>11월 14일 설명회 이후에 이어서 진행합니다. <a href="https://forms.gle/1kwVNaahWW7QWffr8" target="_blank"><strong>설명회 및 시설 참관 신청</strong></a></li>
+  </ul>
+  이 기간 동안 한미유치원을 방문하여 시설을 둘러보고 소개를 받으실 수 있습니다.
+</div>
+<br>
+
+<div class="subtle">
+<a href="https://www.go-firstschool.go.kr" target="_blank">처음학교로</a>의 일정에 맞춰 계속 이 페이지의 내용을 업데이트할 예정입니다.
+  <ul class="flower">
+    <li>우선 모집: 10월 31일 (월) ~ 11월 2일 (수)</li>
+    <li>일반 모집: 11월 16일 (수) ~ 11월 18일 (금)</li>
+  </ul>
+</div>
+-->
+
+<img src="./images/2023/2024_vintro_1.svg" style="width: 100%">
 <br>
 <img src="./images/2023/2024_vintro_2.svg" style="width: 100%">
 <br>
@@ -83,9 +144,9 @@ html['admission'] = `
 <div>그 밖의 정보</div>
 <br>
 <div class="subtle">
-<strong><a href="https://drive.google.com/file/d/1ApPpTBmcWLjJst7RqSw0CMZY2v3GrbAU/view?usp=sharing" target="_blank">설명회 자료의 유아모집 요강</a></strong>에서 관련 제출서류, 방과후과정반, 특성화활동, 통학차량 및 등하원 시간 등의 내용을 확인할 수 있습니다. <strong><a href="https://instagram.com/hanmiucw" target="_blank">한미유치원 인스타그램</a></strong>을 통해 2024학년도 현재 환경 사진과 간추린 교실 이야기 등 설명회 자료에 넣지 못한 다양한 이미지를 공유하고 있습니다.<br><br>
+<strong><a href="https://drive.google.com/file/d/1ApPpTBmcWLjJst7RqSw0CMZY2v3GrbAU/view?usp=sharing" target="_blank">설명회 자료의 유아모집 요강</a></strong>에서 관련 제출서류, 방과후과정반, 특성화활동, 통학차량 및 등하원 시간 등의 내용을 확인할 수 있습니다. <strong><a href="https://instagram.com/hanmiucw" target="_blank">한미유치원 인스타그램</a></strong>을 통해 2023학년도 현재 환경 사진과 간추린 교실 이야기 등 설명회 자료에 넣지 못한 다양한 이미지를 공유하고 있습니다.<br><br>
   <ul class="musical">
-    <li>2025학년도 <strong>방과후 과정 참여</strong>가 필요한 맞벌이 가정과 돌봄이 필요한 가정은 <strong>누구나 신청</strong>할 수 있습니다.</li>
+    <li>2024학년도 <strong>방과후 과정 참여</strong>가 필요한 맞벌이 가정과 돌봄이 필요한 가정은 <strong>누구나 신청</strong>할 수 있습니다.</li>
   </ul>
   <br>
 </div>
@@ -113,8 +174,8 @@ html['admission'] = `
 <br>
 
 <div class="subtle">
-2025학년도가 아닌 2024학년도 입학의 경우는 아래의 입학원서를 작성해 주세요.<br>
-<strong><a href="https://forms.gle/Kfa7BndVgcBPPWCN8" target="_blank">2024학년도 입학원서 작성</a></strong>
+2024학년도가 아닌 2023학년도 입학의 경우는 아래의 입학원서를 작성해 주세요.<br>
+<strong><a href="https://forms.gle/MoadBAdVCpyqsDQ57" target="_blank">2023학년도 입학원서 작성</a></strong>
 </div>
 <br>
 
